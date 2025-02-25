d68959d6911955c0b4c9a6dc52cf9ab888454f8d
Soumil Sourya Sahu <soumilsaurya98@gmail.com>
2025-02-25 21:39:18 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -5,7 +5,14 @@ Create Databases
 Script Purpose:
     This script creates a new database named 'DataWarehouse'. Additionally, the script sets up three 
     separate databases—'bronze', 'silver', and 'gold'—to organize data at different processing stages.
-	
+
+Single Database (DataWarehouse) with Table Prefixes
+Why?
+
+Easier Data Management – All data stays within one database, avoiding the complexity of switching between multiple databases.
+Better Query Performance – Joins and aggregations across layers are faster when tables are in the same database.
+Clear Layering – Using prefixes (bronze_, silver_, gold_) ensures data is categorized properly.
+Simplifies ETL (Extract, Transform, Load) – Moving data between layers is easier with INSERT INTO SELECT or CREATE TABLE AS SELECT.
 */
 
 -- CREATE DATABASE 'DataWarehouse'
